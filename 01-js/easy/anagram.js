9a1638fe58fedc6285e331aa214ab3dbf9fc88ab
Ashutosh Kumar <ak2417942@gmail.com>
2025-04-03 10:45:33 +0530

    01js easy : anagram.js completed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,20 @@
 */
 
 function isAnagram(str1, str2) {
-
+  /* String are immutable in java script */
+  str1.toUpperCase(); /* this returns a new string , it does not change the original string */
+  str2.toUpperCase();
+  console.log(str1);
+  console.log(str2);
+  return (
+    str1.toUpperCase().split("").sort().join("") ===
+    str2.toUpperCase().split("").sort().join("")
+  );
 }
+
+let str1 = "Debit Card";
+let str2 = "Bad Credit";
+
+isAnagram(str1, str2);
 
 module.exports = isAnagram;
